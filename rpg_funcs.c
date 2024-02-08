fbee392e8f9700d15281b159e6ddc45ad34eefe8
yanalg <94481839+yanalg@users.noreply.github.com>
2024-02-08 17:22:14 +0200

    gonna do some major changes
    backup version in case it all goes wrong

--- rpg_funcs.c
+++ rpg_funcs.c
@@ -15,15 +15,38 @@
 #define NOT_A_MEMBER 0
 #define MEMBER 1
 
+// global head of list
+static struct list_head pid_list;
+static int list_initialized_flag = 0;
 
+
+/* list initialization function */
+void init_pid_list(void) {
+    if (!list_initialized_flag) {
+        INIT_LIST_HEAD(&pid_list);
+        list_initialized_flag = 1;
+    }
+}
+
+/* this function checks if a process has a character
+	returns 0 if it doesn't and 1 if it has */
+int has_character(void){
+	pid_t my_pid = getpid();
+	if(pid_in_list(my_pid)){
+		return 1;
+	}
+	return 0;
+}
 
 /* user POV , need to implement wrapper function */
 
 int sys_rpg_create_character(int cclass){
+	//init pid list or make sure it is initialized
+	 init_pid_list();
 	//check if process has a character
-	struct task_struct *current_task = current;
-    struct player *character;
-	if(current_task->player){
+	//struct task_struct *current_task = current;
+	struct player *character;
+	if(!has_character()){
 		//process already has a character
 		errno = EEXIST;
 		return ERROR;
@@ -54,7 +77,9 @@ int sys_rpg_create_character(int cclass){
 		LIST_HEAD_INIT(&character->my_list);
 		//add character to its own list
 		list_add_tail(&character->my_list, &character->my_list);
-		pcurrent_task->player = character;
+		pid_t my_pid = getpid();
+		add_to_list(my_pid);
+		//pcurrent_task->player = character;
 		return SUCCESS; 
 		
 	}
@@ -64,30 +89,32 @@ int sys_rpg_create_character(int cclass){
 /******************************************************************************************************/
 
 int sys_rpg_fight(int type , int level){
+	//init pid list or make sure it is initialized
+	 init_pid_list();
 	// check if arguments are valid
 	if( level < 0 || !(type == CREATURE_ORC || type == CREATURE_DEMON)){
 		errno = EINVAL;
 		return ERROR;
 	}
 	//check if process has a character
-	struct task_struct *p = current;
-	if(p->ptr){
-		//process already has a character
+	if(!has_character()){
+		//player does not have a character
 		errno = EINVAL;
-		return ERROR;
+		return ERROR;	
 	}
-	
-    
+
+	//struct task_struct *p = current;
+
 	struct rpg_stats * my_party;
-	my_party = malloc(sizeof(struct rpg_stat));
-	if( my_party =! NULL){
+	my_party = kmalloc(sizeof(struct rpg_stats), GFP_KERNEL);
+	if( my_party == NULL){
 		// allocation failed
 		errno = -ENOMEM;
 		return ERROR;
 		
 	}
 	if(rpg_get_stats(my_party) == -1){
-		free(my_party);
+		kfree(my_party);
 		return ERROR;
 	}
 		
@@ -98,10 +125,8 @@ int sys_rpg_fight(int type , int level){
 		list_for_each_entry(entry,&(p->ptr->my_list),p->ptr->my_list){
 			(entry->player_level)++;
 		}
-		free(my_party);
-		return WIN;
-			
-			
+		kfree(my_party);
+		return WIN;		
 	}
 	else{
 		//party lost
@@ -113,7 +138,7 @@ int sys_rpg_fight(int type , int level){
 					
 			}
 		}
-		free(my_party);
+		kfree(my_party);
 		return LOSE;
 			
 	}
@@ -140,11 +165,11 @@ int sys_rpg_get_stats(struct rpg_stats* stats){
 		return ERROR;
 	}
 
-	struct task_struct *p = current;
-	if(p->ptr == NULL){
-		//process does not havs a character
+	//struct task_struct *p = current;
+	if(!has_character()){
+		//player does not have a character
 		errno = EINVAL;
-		return ERROR;
+		return ERROR;	
 	}
 	//checking if a user-space pointer is valid
 	if (access_ok(stats, sizeof(struct rpg_stats), VERIFY_WRITE) == 0) {
@@ -159,13 +184,13 @@ int sys_rpg_get_stats(struct rpg_stats* stats){
 	stats->fighter_levels = 0;
 	stats->mage_levels = 0;
 	
-	
+
 	list_for_each_entry(entry,&(p->ptr->my_list),p->ptr->my_list){
 		(stats->party_size)++;
-		if(entry->cclass == MAGE ){
+		if(entry->cclass == MAGE){
 			stats->mage_levels += entry->player_level;
 		}
-		if( entry->cclass == FIGHTER){
+		if(entry->cclass == FIGHTER){
 			stats->fighter_levels += entry->player_level;
 		}		
 		
@@ -199,6 +224,54 @@ int sys_rpg_join(pid_t player){
 	
 	return SUCCESS;
 	
+}
+/*********************************************************************************/
+/*add node to list 
+  returns 0 on success, -1 on fail*/
+int add_to_list(pid_t pid) {
+	init_pid_list();
+    struct pid_node *new_node;
+
+    new_node = kmalloc(sizeof(struct pid_node), GFP_KERNEL);
+    if (!new_node){
+		//allocation failed
+		return -ENOMEM;
+	}
+        
+    new_node->pid = pid;
+    list_add_tail(&new_node->list, &pid_list);
+
+    return 0;
+}
+
+
+/* check if pid is in list
+	returns 1 if it is in the list, 0 otherwise */
+int pid_in_list(pid_t pid) {
+	init_pid_list();
+    struct pid_node *entry;
+
+    list_for_each_entry(entry, &pid_list, list) {
+        if (entry->pid == pid)
+            return 1;
+    }
+
+    return 0;
+}
+
+
+// Remove a PID from the list
+void remove_from_list(pid_t pid) {
+	init_pid_list();
+    struct pid_node *entry, *tmp;
+
+    list_for_each_entry_safe(entry, tmp, &pid_list, list) {
+        if (entry->pid == pid) {
+            list_del(&entry->list);
+            kfree(entry);
+            break;
+        }
+    }
 }
 
 
--- rpg_funcs.h
+++ rpg_funcs.h
@@ -16,6 +16,11 @@ int sys_rpg_create_character(int cclass);
 int sys_rpg_fight(int type , int level);
 int sys_rpg_get_stats(struct rpg_stats* stats);
 int sys_rpg_join(pid_t player);
+void init_pid_list(void);
+int add_to_list(pid_t pid);
+int pid_in_list(pid_t pid);
+void remove_from_list(pid_t pid);
+
 
 
 /* create player struct*/
@@ -25,6 +30,13 @@ struct player {
 	int party_member;
 	list_t my_list;
 	
+};
+
+/* create a struct to store the pid's of the processes
+   that have a characters */
+struct pid_node {
+    pid_t pid;
+    struct list_head list;
 };
 
 
